813178d18a9acc06e5de8f2b8085c4923bfac26f
Propietario <Propietario@Propietario-PC>
2020-06-26 19:11:58 -0500

    Merge Testing

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Propietario
+ *
+ * Created on 26 de junio de 2020, 06:19 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int aux = 15;
+    
+    printf("Hola %d",aux);
+    
+    return (EXIT_SUCCESS);
+}
+
